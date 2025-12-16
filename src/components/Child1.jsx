0385e40c42ec2c749fc281f8f6384c1e0efac2e6
Shiva-Prasad83 <shivaprasad.ch83@gmail.com>
2025-12-16 16:18:49 +0530

    Create Child1.jsx

---
+++ src/components/Child1.jsx
@@ -0,0 +1,13 @@
+
+function Child1({setSelectedOption}){
+
+  return (
+    <div style={{backgroundColor:"#C6671A",width:"250px" height:"120px"}}>
+    <h1>Child Component 1</h1>
+
+      <button onClick={()=>setSelectedOption("Option 1")}>Option 1</button>
+    </div>
+  )
+}
+
+export default Child1;
